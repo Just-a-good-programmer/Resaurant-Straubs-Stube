556907190d645a16c21dcec939d0c18c0731c179
smth1808 <MB.Straub@gmail.com>
2026-02-24 11:19:04 +0100

    eerste opzet project

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+    if (isset($_SESSION['username'])) {
+        header("Location: homepage.php");
+        exit();
+    }
+    include 'connecties database/conn.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    
+</body>
+</html>
